6d573e5b22653442d29c6af53922eb63d03b130d
Andre Vincenti <avincent@akamai.com>
2025-11-17 13:34:50 -0600

    Homework #1

--- main.tf
+++ main.tf
@@ -2,3 +2,30 @@ data "akamai_group" "andre_terraform_test" {
     group_name = "VidyaTest"
     contract_id = "1-3CV382"
 }
+
+output "example" {
+
+    value = data.akamai_group.andre_terraform_test.id
+}
+
+data "akamai_appsec_configuration" "andre_terraform_test_sec" {
+
+}
+
+
+data "akamai_appsec_configuration" "app_sec_sample"{
+    name = "AMTest"
+}
+
+output "sec_config_sample" {
+    value = data.akamai_appsec_configuration.app_sec_sample.config_id
+}
+
+data "akamai_property" "andre_terraform_property" {
+    name = "ssltest"
+    version = "3"
+}
+
+output "propety_example" {
+    value = data.akamai_property.andre_terraform_property.name
+}
